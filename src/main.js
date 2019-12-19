33578114600d5490030784989513701de3a20411
自己的用户名 <992049747@qq.com>
2019-12-19 08:20:26 +0800

    xiaoaizaixian

--- src/main.js
+++ src/main.js
@@ -2,11 +2,31 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import dayjs from 'dayjs'
+import service from "./http"
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import VCharts from 'v-charts'
+import 'lodash'
+import 'mavon-editor/dist/css/index.css'
+import countTo from 'vue-count-to';
+import 'vue-image-crop-upload'
+import JsonExcel from 'vue-json-excel'
 
+Vue.use(ElementUI)
+Vue.use(Vant);
+Vue.use(VCharts);
+Vue.use(countTo);
+Vue.component('downloadExcel', JsonExcel)
+Vue.prototype.$axios = service
+Vue.prototype.$dayjs = dayjs
 Vue.config.productionTip = false
 
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
